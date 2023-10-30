279594ec561c65761d8ca28506eeb0357ec81e45
IrshadFlutter <m38648747@gmail.com>
2023-10-30 13:36:42 +0530

    Screen2

--- lib/Screen1.dart
+++ lib/Screen1.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import './Screen2.dart';
 
 class Screen1 extends StatefulWidget {
   const Screen1({Key? key}) : super(key: key);
@@ -13,10 +14,52 @@ class _Screen1State extends State<Screen1> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xff53B175),
-
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-
+          SizedBox(
+            height: 250,
+          ),
+          Center(
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                    left: 73,
+                  ),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (_) => Screen2()));
+                    },
+                    child: Image.asset(
+                      "assets/a.png",
+                      width: 50.1267204284668.w,
+                      height: 80.12504196166992.h,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    children: [
+                      Text("nectar",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white,
+                          )),
+                      Text("online groceriet",
+                          style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white)),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
